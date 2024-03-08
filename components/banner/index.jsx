87f10a949382183ed3c336f52584771b234250fc
Tri Tran <tri.tranthien@hdwebsoft.dev>
2024-03-08 11:57:41 +0700

    banner2

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -7,7 +7,7 @@ const Banner = ({info}) => {
     return (
         <div className="md:min-h-[550px] h-svh relative bg-white">
             <Image
-                src="/banner.jpg"
+                src="/banner2.jpg"
                 alt="trăm năm hạnh phúc"
                 layout="fill"
                 objectFit='cover'
